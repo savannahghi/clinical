ee66d72f66fb80b44a64af6a415b1afaecc9245d
Richard Ochom <ochomrichard752@gmail.com>
2021-08-31 13:42:09 +0300

    chore: fix tests for register user mutation (#26)

---
+++ graph/entity.resolvers.go
@@ -0,0 +1,21 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/savannahghi/clinical/graph/clinical"
+	"github.com/savannahghi/clinical/graph/generated"
+)
+
+func (r *entityResolver) FindDummyByID(ctx context.Context, id string) (*clinical.Dummy, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Entity returns generated.EntityResolver implementation.
+func (r *Resolver) Entity() generated.EntityResolver { return &entityResolver{r} }
+
+type entityResolver struct{ *Resolver }
