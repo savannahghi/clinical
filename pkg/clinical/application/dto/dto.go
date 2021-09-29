9be2c7d2271cb9a81a1d1d8f628af21604a75a0f
Otieno Calvine <35563516+NYARAS@users.noreply.github.com>
2021-09-29 17:02:28 +0300

    chore: port onbaording isc implementation into there own usecases (#40)
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- pkg/clinical/application/dto/dto.go
+++ pkg/clinical/application/dto/dto.go
@@ -1,1 +1,17 @@
 package dto
+
+import (
+	"github.com/savannahghi/enumutils"
+	"github.com/savannahghi/scalarutils"
+)
+
+//RegisterUserPayload is the payload used to pass user details to onboarding service
+type RegisterUserPayload struct {
+	UID         string           `json:"uid,omitempty"`
+	FirstName   string           `json:"firstName,omitempty"`
+	LastName    string           `json:"lastName,omitempty"`
+	Gender      enumutils.Gender `json:"gender,omitempty"`
+	PhoneNumber string           `json:"phoneNumber,omitempty"`
+	Email       string           `json:"email,omitempty"`
+	DateOfBirth scalarutils.Date `json:"dateOfBirth,omitempty"`
+}
