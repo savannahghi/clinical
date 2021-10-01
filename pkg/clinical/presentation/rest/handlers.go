1c778155c5576fbe9bbc7e693330eed907fa5e16
Otieno Calvine <nyarangaotieno@gmail.com>
2021-10-01 14:40:45 +0300

    chore: add more critical acceptance testing
    Critical GraphQL patient related endpoints:
     - TestGraphQLEndEpisode
     - TestGraphQLStartEpisode
     - TestGraphQLStartEpisodeByBreakGlass
     - TestGraphQLStartEpisodeByOTP
     - TestGraphQGetPatient
     - TestGraphQLFindPatients
     - TestGraphQFindPatientsByMSISDN
    
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

---
+++ pkg/clinical/presentation/rest/handlers.go
@@ -0,0 +1,72 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/savannahghi/clinical/pkg/clinical/domain"
+	"github.com/savannahghi/clinical/pkg/clinical/infrastructure"
+	"github.com/savannahghi/clinical/pkg/clinical/usecases"
+	"github.com/savannahghi/serverutils"
+)
+
+// PresentationHandlers represents all the REST API logic
+type PresentationHandlers interface {
+	DeleteFHIRPatientByPhone() http.HandlerFunc
+}
+
+// PresentationHandlersImpl represents the usecase implementation object
+type PresentationHandlersImpl struct {
+	usecases       usecases.Interactor
+	infrastructure infrastructure.Infrastructure
+}
+
+// NewPresentationHandlers initializes a new rest handlers usecase
+func NewPresentationHandlers(infrastructure infrastructure.Infrastructure, usecases usecases.Interactor) PresentationHandlers {
+	return &PresentationHandlersImpl{infrastructure: infrastructure, usecases: usecases}
+}
+
+// DeleteFHIRPatientByPhone handler exposes an endpoint that takes a
+// patient's phone number and deletes the patient's FHIR compartment
+func (p PresentationHandlersImpl) DeleteFHIRPatientByPhone() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		payload := &domain.PhoneNumberPayload{}
+		type errResponse struct {
+			Err string `json:"error"`
+		}
+		serverutils.DecodeJSONToTargetStruct(w, r, payload)
+		if payload.PhoneNumber == "" {
+			serverutils.WriteJSONResponse(
+				w,
+				errResponse{
+					Err: "expected a phone number to be defined",
+				},
+				http.StatusBadRequest,
+			)
+			return
+		}
+		deleted, err := p.usecases.DeleteFHIRPatientByPhone(ctx, payload.PhoneNumber)
+		if err != nil {
+			err := fmt.Sprintf("unable to delete patient: %v", err.Error())
+			serverutils.WriteJSONResponse(
+				w,
+				errResponse{
+					Err: err,
+				},
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
+		type response struct {
+			Deleted bool `json:"deleted"`
+		}
+		serverutils.WriteJSONResponse(
+			w,
+			response{Deleted: deleted},
+			http.StatusOK,
+		)
+	}
+}
