23f50491d21c1a4dfaccd4843c17d930296f661d
Salaton <nairouasalaton@gmail.com>
2022-03-21 14:59:22 +0300

    feat: setup pubsub service

--- pkg/clinical/application/extensions/extension.go
+++ pkg/clinical/application/extensions/extension.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"net/http"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/savannahghi/converterandformatter"
 	"github.com/savannahghi/firebasetools"
 	"github.com/savannahghi/interserviceclient"
 	"github.com/savannahghi/profileutils"
+	"github.com/savannahghi/pubsubtools"
 	"github.com/savannahghi/serverutils"
 )
 
@@ -47,6 +49,41 @@ type BaseExtension interface {
 		source interface{},
 		status int,
 	)
+
+	// PubSub
+	EnsureTopicsExist(
+		ctx context.Context,
+		pubsubClient *pubsub.Client,
+		topicIDs []string,
+	) error
+	NamespacePubsubIdentifier(
+		serviceName string,
+		topicID string,
+		environment string,
+		version string,
+	) string
+	PublishToPubsub(
+		ctx context.Context,
+		pubsubClient *pubsub.Client,
+		topicID string,
+		environment string,
+		serviceName string,
+		version string,
+		payload []byte,
+	) error
+	EnsureSubscriptionsExist(
+		ctx context.Context,
+		pubsubClient *pubsub.Client,
+		topicSubscriptionMap map[string]string,
+		callbackURL string,
+	) error
+	SubscriptionIDs(topicIDs []string) map[string]string
+	PubSubHandlerPath() string
+	VerifyPubSubJWTAndDecodePayload(
+		w http.ResponseWriter,
+		r *http.Request,
+	) (*pubsubtools.PubSubPayload, error)
+	GetPubSubTopic(m *pubsubtools.PubSubPayload) (string, error)
 }
 
 // BaseExtensionImpl ...
@@ -105,4 +142,98 @@ func (b *BaseExtensionImpl) WriteJSONResponse(
 // ErrorMap turns the supplied error into a map with "error" as the key
 func (b *BaseExtensionImpl) ErrorMap(err error) map[string]string {
 	return serverutils.ErrorMap(err)
+}
+
+// EnsureTopicsExist creates the topic(s) in the suppplied list if they do not
+// already exist.
+func (b *BaseExtensionImpl) EnsureTopicsExist(
+	ctx context.Context,
+	pubsubClient *pubsub.Client,
+	topicIDs []string,
+) error {
+	return pubsubtools.EnsureTopicsExist(ctx, pubsubClient, topicIDs)
+}
+
+// NamespacePubsubIdentifier uses the service name, environment and version to
+// create a "namespaced" pubsub identifier. This could be a topicID or
+// subscriptionID.
+func (b *BaseExtensionImpl) NamespacePubsubIdentifier(
+	serviceName string,
+	topicID string,
+	environment string,
+	version string,
+) string {
+	return pubsubtools.NamespacePubsubIdentifier(
+		serviceName,
+		topicID,
+		environment,
+		version,
+	)
+}
+
+// PublishToPubsub sends the supplied payload to the indicated topic
+func (b *BaseExtensionImpl) PublishToPubsub(
+	ctx context.Context,
+	pubsubClient *pubsub.Client,
+	topicID string,
+	environment string,
+	serviceName string,
+	version string,
+	payload []byte,
+) error {
+	return pubsubtools.PublishToPubsub(
+		ctx,
+		pubsubClient,
+		topicID,
+		environment,
+		serviceName,
+		version,
+		payload,
+	)
+}
+
+// EnsureSubscriptionsExist ensures that the subscriptions named in the supplied
+// topic:subscription map exist. If any does not exist, it is created.
+func (b *BaseExtensionImpl) EnsureSubscriptionsExist(
+	ctx context.Context,
+	pubsubClient *pubsub.Client,
+	topicSubscriptionMap map[string]string,
+	callbackURL string,
+) error {
+	return pubsubtools.EnsureSubscriptionsExist(
+		ctx,
+		pubsubClient,
+		topicSubscriptionMap,
+		callbackURL,
+	)
+}
+
+// SubscriptionIDs returns a map of topic IDs to subscription IDs
+func (b *BaseExtensionImpl) SubscriptionIDs(topicIDs []string) map[string]string {
+	return pubsubtools.SubscriptionIDs(topicIDs)
+}
+
+// PubSubHandlerPath returns pubsub hander path `/pubsub`
+func (b *BaseExtensionImpl) PubSubHandlerPath() string {
+	return pubsubtools.PubSubHandlerPath
+}
+
+// VerifyPubSubJWTAndDecodePayload confirms that there is a valid Google signed
+// JWT and decodes the pubsub message payload into a struct.
+//
+// It's use will simplify & shorten the handler funcs that process Cloud Pubsub
+// push notifications.
+func (b *BaseExtensionImpl) VerifyPubSubJWTAndDecodePayload(
+	w http.ResponseWriter,
+	r *http.Request,
+) (*pubsubtools.PubSubPayload, error) {
+	return pubsubtools.VerifyPubSubJWTAndDecodePayload(
+		w,
+		r,
+	)
+}
+
+// GetPubSubTopic retrieves a pubsub topic from a pubsub payload.
+func (b *BaseExtensionImpl) GetPubSubTopic(m *pubsubtools.PubSubPayload) (string, error) {
+	return pubsubtools.GetPubSubTopic(m)
 }
---
+++ pkg/clinical/infrastructure/services/pubsub/subscriber.go
@@ -0,0 +1,48 @@
+package pubsubmessaging
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/savannahghi/errorcodeutil"
+	"github.com/savannahghi/serverutils"
+)
+
+// ReceivePubSubPushMessages receives and processes a pubsub message
+func (ps ServicePubSubMessaging) ReceivePubSubPushMessages(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	message, err := ps.baseExt.VerifyPubSubJWTAndDecodePayload(w, r)
+	if err != nil {
+		serverutils.WriteJSONResponse(w, errorcodeutil.CustomError{
+			Err:     err,
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	topicID, err := ps.baseExt.GetPubSubTopic(message)
+	if err != nil {
+		serverutils.WriteJSONResponse(w, errorcodeutil.CustomError{
+			Err:     err,
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	switch topicID {
+	// Listen to the topics here
+	}
+
+	resp := map[string]string{"Status": "Success"}
+	returnedResponse, err := json.Marshal(resp)
+	if err != nil {
+		serverutils.WriteJSONResponse(w, errorcodeutil.CustomError{
+			Err:     err,
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+	_, _ = w.Write(returnedResponse)
+}
