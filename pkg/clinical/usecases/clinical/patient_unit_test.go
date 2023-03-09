59374ad220a61014048c99fafe38e240173f0d54
Salaton <nairouasalaton@gmail.com>
2023-03-09 16:02:28 +0300

    tests: add sad cases to usecase unit tests

--- pkg/clinical/usecases/clinical/patient_unit_test.go
+++ pkg/clinical/usecases/clinical/patient_unit_test.go
@@ -12,6 +12,7 @@ import (
 	fakeFHIRMock "github.com/savannahghi/clinical/pkg/clinical/infrastructure/datastore/cloudhealthcare/mock"
 	fakeOCLMock "github.com/savannahghi/clinical/pkg/clinical/infrastructure/services/openconceptlab/mock"
 	clinicalUsecase "github.com/savannahghi/clinical/pkg/clinical/usecases/clinical"
+	"github.com/savannahghi/firebasetools"
 	"github.com/segmentio/ksuid"
 )
 
@@ -121,15 +122,195 @@ func TestClinicalUseCaseImpl_PatientTimeline(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "Sad Case - Fail to search allergy intolerance",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to get allergy intolerance - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully get allergy intolerance",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to search observation",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully get observation",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to get observation - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to search medication statement",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully get medication statement",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to search medication statement - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+				count:     4,
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			FakeExt := fakeExtMock.NewFakeBaseExtensionMock()
-			Fakefhir := fakeFHIRMock.NewFHIRMock()
-			FakeOCL := fakeOCLMock.NewFakeOCLMock()
+			fakeExt := fakeExtMock.NewFakeBaseExtensionMock()
+			fakeFHIR := fakeFHIRMock.NewFHIRMock()
+			fakeOCL := fakeOCLMock.NewFakeOCLMock()
 
-			infra := infrastructure.NewInfrastructureInteractor(FakeExt, Fakefhir, FakeOCL)
+			infra := infrastructure.NewInfrastructureInteractor(fakeExt, fakeFHIR, fakeOCL)
 			u := clinicalUsecase.NewUseCasesClinicalImpl(infra)
+
+			if tt.name == "Sad Case - Fail to search allergy intolerance" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return &domain.FHIRAllergyIntoleranceRelayConnection{}, fmt.Errorf("failed to search allergy")
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully get allergy intolerance" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return &domain.FHIRAllergyIntoleranceRelayConnection{
+						Edges: []*domain.FHIRAllergyIntoleranceRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRAllergyIntolerance{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to get allergy intolerance - nil node" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return &domain.FHIRAllergyIntoleranceRelayConnection{
+						Edges: []*domain.FHIRAllergyIntoleranceRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search observation" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return &domain.FHIRObservationRelayConnection{}, fmt.Errorf("failed to get observation")
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully get observation" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return &domain.FHIRObservationRelayConnection{
+						Edges: []*domain.FHIRObservationRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRObservation{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to get observation - nil node" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return &domain.FHIRObservationRelayConnection{
+						Edges: []*domain.FHIRObservationRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search medication statement" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return &domain.FHIRMedicationStatementRelayConnection{}, fmt.Errorf("failed to get medication statement")
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully get medication statement" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return &domain.FHIRMedicationStatementRelayConnection{
+						Edges: []*domain.FHIRMedicationStatementRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRMedicationStatement{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search medication statement - nil node" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return &domain.FHIRMedicationStatementRelayConnection{
+						Edges: []*domain.FHIRMedicationStatementRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
 
 			got, err := u.PatientTimeline(tt.args.ctx, tt.args.patientID, tt.args.count)
 			if (err != nil) != tt.wantErr {
@@ -237,15 +418,186 @@ func TestClinicalUseCaseImpl_GetMedicalData(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "Sad Case - Fail to search medication statement",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Sad Case - Fail to search medication statement - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully search medication statement",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to search allergy intolerance",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Sad Case - Fail to search allergy intolerance - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully search allergy intolerance",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case - Fail to search observation",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Sad Case - Fail to search observation - nil node",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Happy Case - Successfully search observation",
+			args: args{
+				ctx:       ctx,
+				patientID: gofakeit.UUID(),
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			FakeExt := fakeExtMock.NewFakeBaseExtensionMock()
-			Fakefhir := fakeFHIRMock.NewFHIRMock()
-			FakeOCL := fakeOCLMock.NewFakeOCLMock()
+			fakeExt := fakeExtMock.NewFakeBaseExtensionMock()
+			fakeFHIR := fakeFHIRMock.NewFHIRMock()
+			fakeOCL := fakeOCLMock.NewFakeOCLMock()
 
-			infra := infrastructure.NewInfrastructureInteractor(FakeExt, Fakefhir, FakeOCL)
+			infra := infrastructure.NewInfrastructureInteractor(fakeExt, fakeFHIR, fakeOCL)
 			u := clinicalUsecase.NewUseCasesClinicalImpl(infra)
+
+			if tt.name == "Sad Case - Fail to search medication statement" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return nil, fmt.Errorf("failed to search medication statement")
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search medication statement - nil node" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return &domain.FHIRMedicationStatementRelayConnection{
+						Edges: []*domain.FHIRMedicationStatementRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully search medication statement" {
+				fakeFHIR.MockSearchFHIRMedicationStatementFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRMedicationStatementRelayConnection, error) {
+					return &domain.FHIRMedicationStatementRelayConnection{
+						Edges: []*domain.FHIRMedicationStatementRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRMedicationStatement{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search allergy intolerance" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return nil, fmt.Errorf("failed to search allergy intolerance")
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search allergy intolerance - nil node" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return &domain.FHIRAllergyIntoleranceRelayConnection{
+						Edges: []*domain.FHIRAllergyIntoleranceRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully search allergy intolerance" {
+				fakeFHIR.MockSearchFHIRAllergyIntoleranceFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRAllergyIntoleranceRelayConnection, error) {
+					return &domain.FHIRAllergyIntoleranceRelayConnection{
+						Edges: []*domain.FHIRAllergyIntoleranceRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRAllergyIntolerance{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search observation" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return nil, fmt.Errorf("failed to search observation")
+				}
+			}
+
+			if tt.name == "Sad Case - Fail to search observation - nil node" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return &domain.FHIRObservationRelayConnection{
+						Edges: []*domain.FHIRObservationRelayEdge{
+							{
+								Cursor: new(string),
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
+
+			if tt.name == "Happy Case - Successfully search observation" {
+				fakeFHIR.MockSearchFHIRObservationFn = func(ctx context.Context, params map[string]interface{}) (*domain.FHIRObservationRelayConnection, error) {
+					return &domain.FHIRObservationRelayConnection{
+						Edges: []*domain.FHIRObservationRelayEdge{
+							{
+								Cursor: new(string),
+								Node:   &domain.FHIRObservation{},
+							},
+						},
+						PageInfo: &firebasetools.PageInfo{},
+					}, nil
+				}
+			}
 
 			got, err := u.GetMedicalData(tt.args.ctx, tt.args.patientID)
 			if (err != nil) != tt.wantErr {
