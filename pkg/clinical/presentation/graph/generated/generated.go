09b6c614f56c83db7eb39c6b4de06585de75467b
Salaton <nairouasalaton@gmail.com>
2023-03-23 11:21:54 +0300

    feat: get patient's respiratory rate entries (#205)

--- pkg/clinical/presentation/graph/generated/generated.go
+++ pkg/clinical/presentation/graph/generated/generated.go
@@ -369,14 +369,15 @@ type ComplexityRoot struct {
 	}
 
 	Query struct {
-		GetEpisodeOfCare               func(childComplexity int, id string) int
-		GetMedicalData                 func(childComplexity int, patientID string) int
-		GetPatientBloodPressureEntries func(childComplexity int, patientID string) int
-		GetPatientHeightEntries        func(childComplexity int, patientID string) int
-		GetPatientTemperatureEntries   func(childComplexity int, patientID string) int
-		ListPatientEncounters          func(childComplexity int, patientID string) int
-		PatientHealthTimeline          func(childComplexity int, input dto.HealthTimelineInput) int
-		__resolve__service             func(childComplexity int) int
+		GetEpisodeOfCare                 func(childComplexity int, id string) int
+		GetMedicalData                   func(childComplexity int, patientID string) int
+		GetPatientBloodPressureEntries   func(childComplexity int, patientID string) int
+		GetPatientHeightEntries          func(childComplexity int, patientID string) int
+		GetPatientRespiratoryRateEntries func(childComplexity int, patientID string) int
+		GetPatientTemperatureEntries     func(childComplexity int, patientID string) int
+		ListPatientEncounters            func(childComplexity int, patientID string) int
+		PatientHealthTimeline            func(childComplexity int, input dto.HealthTimelineInput) int
+		__resolve__service               func(childComplexity int) int
 	}
 
 	TimelineResource struct {
@@ -416,6 +417,7 @@ type QueryResolver interface {
 	GetPatientTemperatureEntries(ctx context.Context, patientID string) ([]*dto.Observation, error)
 	GetPatientBloodPressureEntries(ctx context.Context, patientID string) ([]*dto.Observation, error)
 	GetPatientHeightEntries(ctx context.Context, patientID string) ([]*dto.Observation, error)
+	GetPatientRespiratoryRateEntries(ctx context.Context, patientID string) ([]*dto.Observation, error)
 }
 
 type executableSchema struct {
@@ -2023,6 +2025,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 
 		return e.complexity.Query.GetPatientHeightEntries(childComplexity, args["patientID"].(string)), true
 
+	case "Query.getPatientRespiratoryRateEntries":
+		if e.complexity.Query.GetPatientRespiratoryRateEntries == nil {
+			break
+		}
+
+		args, err := ec.field_Query_getPatientRespiratoryRateEntries_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Query.GetPatientRespiratoryRateEntries(childComplexity, args["patientID"].(string)), true
+
 	case "Query.getPatientTemperatureEntries":
 		if e.complexity.Query.GetPatientTemperatureEntries == nil {
 			break
@@ -2224,6 +2238,7 @@ var sources = []*ast.Source{
   getPatientTemperatureEntries(patientID: String!): [Observation!]
   getPatientBloodPressureEntries(patientID: String!): [Observation!]
   getPatientHeightEntries(patientID: String!): [Observation!]
+  getPatientRespiratoryRateEntries(patientID: String!): [Observation!]
 }
 
 extend type Mutation {
@@ -4546,6 +4561,21 @@ func (ec *executionContext) field_Query_getPatientBloodPressureEntries_args(ctx 
 }
 
 func (ec *executionContext) field_Query_getPatientHeightEntries_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 string
+	if tmp, ok := rawArgs["patientID"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("patientID"))
+		arg0, err = ec.unmarshalNString2string(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["patientID"] = arg0
+	return args, nil
+}
+
+func (ec *executionContext) field_Query_getPatientRespiratoryRateEntries_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
 	var err error
 	args := map[string]interface{}{}
 	var arg0 string
@@ -14805,6 +14835,72 @@ func (ec *executionContext) fieldContext_Query_getPatientHeightEntries(ctx conte
 	return fc, nil
 }
 
+func (ec *executionContext) _Query_getPatientRespiratoryRateEntries(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Query_getPatientRespiratoryRateEntries(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Query().GetPatientRespiratoryRateEntries(rctx, fc.Args["patientID"].(string))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		return graphql.Null
+	}
+	res := resTmp.([]*dto.Observation)
+	fc.Result = res
+	return ec.marshalOObservation2ᚕᚖgithubᚗcomᚋsavannahghiᚋclinicalᚋpkgᚋclinicalᚋapplicationᚋdtoᚐObservationᚄ(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Query_getPatientRespiratoryRateEntries(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Query",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			switch field.Name {
+			case "id":
+				return ec.fieldContext_Observation_id(ctx, field)
+			case "status":
+				return ec.fieldContext_Observation_status(ctx, field)
+			case "patientID":
+				return ec.fieldContext_Observation_patientID(ctx, field)
+			case "encounterID":
+				return ec.fieldContext_Observation_encounterID(ctx, field)
+			case "name":
+				return ec.fieldContext_Observation_name(ctx, field)
+			case "value":
+				return ec.fieldContext_Observation_value(ctx, field)
+			}
+			return nil, fmt.Errorf("no field named %q was found under type Observation", field.Name)
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Query_getPatientRespiratoryRateEntries_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return
+	}
+	return fc, nil
+}
+
 func (ec *executionContext) _Query__service(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_Query__service(ctx, field)
 	if err != nil {
@@ -20941,6 +21037,26 @@ func (ec *executionContext) _Query(ctx context.Context, sel ast.SelectionSet) gr
 					}
 				}()
 				res = ec._Query_getPatientHeightEntries(ctx, field)
+				return res
+			}
+
+			rrm := func(ctx context.Context) graphql.Marshaler {
+				return ec.OperationContext.RootResolverMiddleware(ctx, innerFunc)
+			}
+
+			out.Concurrently(i, func() graphql.Marshaler {
+				return rrm(innerCtx)
+			})
+		case "getPatientRespiratoryRateEntries":
+			field := field
+
+			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._Query_getPatientRespiratoryRateEntries(ctx, field)
 				return res
 			}
 
