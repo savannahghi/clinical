5d7d46b08276eedd4ab2d45928780aaacefb74d1
Charles Muchogo <dev@muchogo.com>
2023-04-27 12:36:29 +0300

    fix: synced medical data (#235)

---
+++ pkg/clinical/application/dto/observation_output.go
@@ -0,0 +1,44 @@
+package dto
+
+// Observation is a minimal representation of a fhir Observation
+type Observation struct {
+	ID          string            `json:"id,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	Value       string            `json:"value,omitempty"`
+	Status      ObservationStatus `json:"status,omitempty"`
+	PatientID   string            `json:"patientID,omitempty"`
+	EncounterID string            `json:"encounterID,omitempty"`
+}
+
+// ObservationEdge is an observation edge
+type ObservationEdge struct {
+	Node   Observation
+	Cursor string
+}
+
+// ObservationConnection  is an Observation Connection Type
+type ObservationConnection struct {
+	TotalCount int
+	Edges      []ObservationEdge
+	PageInfo   PageInfo
+}
+
+// CreateConditionConnection creates a connection that follows the GraphQl Cursor Connection Specification
+func CreateObservationConnection(observations []*Observation, pageInfo PageInfo, total int) ObservationConnection {
+	connection := ObservationConnection{
+		TotalCount: total,
+		Edges:      []ObservationEdge{},
+		PageInfo:   pageInfo,
+	}
+
+	for _, observation := range observations {
+		edge := ObservationEdge{
+			Node:   *observation,
+			Cursor: observation.ID,
+		}
+
+		connection.Edges = append(connection.Edges, edge)
+	}
+
+	return connection
+}
