ee624db4aa635cfd1bdf9f830d5420716c4d7e22
EspiraMarvin <espiramarvin@gmail.com>
2023-07-26 14:01:11 +0300

    expose muac patient vital query (#267)

--- pkg/clinical/usecases/usecases.go
+++ pkg/clinical/usecases/usecases.go
@@ -62,6 +62,7 @@ type Clinical interface {
 	GetPatientPulseRateEntries(ctx context.Context, patientID string, pagination *dto.Pagination) (*dto.ObservationConnection, error)
 	GetPatientBMIEntries(ctx context.Context, patientID string, pagination *dto.Pagination) (*dto.ObservationConnection, error)
 	GetPatientWeightEntries(ctx context.Context, patientID string, pagination *dto.Pagination) (*dto.ObservationConnection, error)
+	GetPatientMuacEntries(ctx context.Context, patientID string, pagination *dto.Pagination) (*dto.ObservationConnection, error)
 	GetPatientViralLoad(ctx context.Context, patientID string, pagination *dto.Pagination) (*dto.ObservationConnection, error)
 
 	CreateAllergyIntolerance(ctx context.Context, input dto.AllergyInput) (*dto.Allergy, error)
