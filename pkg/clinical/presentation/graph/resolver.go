f8e377d8f44d0dd14a19c8f2fb8253ba7f82d36e
Salaton <nairouasalaton@gmail.com>
2024-02-26 12:41:43 +0300

    chore: purge usecase interactor (#374)

--- pkg/clinical/presentation/graph/resolver.go
+++ pkg/clinical/presentation/graph/resolver.go
@@ -1,19 +1,19 @@
 package graph
 
 import (
-	"github.com/savannahghi/clinical/pkg/clinical/usecases"
+	"github.com/savannahghi/clinical/pkg/clinical/usecases/clinical"
 )
 
 //go:generate go run github.com/99designs/gqlgen
 
 // Resolver wires up the resolvers needed for the clinical services
 type Resolver struct {
-	usecases usecases.Interactor
+	usecases clinical.UseCasesClinicalImpl
 }
 
 // NewResolver initializes a working top leve Resolver that has been initialized
 // with all necessary dependencies
-func NewResolver(usecases usecases.Interactor) (*Resolver, error) {
+func NewResolver(usecases clinical.UseCasesClinicalImpl) (*Resolver, error) {
 	return &Resolver{
 		usecases: usecases,
 	}, nil
