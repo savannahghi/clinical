83eb15053270df4132c9539d96a51d3069d10726
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2024-03-01 14:42:32 +0300

    fix: start encounter
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- pkg/clinical/domain/encounter.go
+++ pkg/clinical/domain/encounter.go
@@ -284,7 +284,7 @@ type FHIREncounterInput struct {
 	PartOf *FHIRReferenceInput `json:"partOf,omitempty"`
 
 	// Meta stores more information about the resource
-	Meta FHIRMetaInput `json:"meta,omitempty"`
+	Meta *FHIRMetaInput `json:"meta,omitempty"`
 
 	// Extension is an optional element that provides additional information not captured in the basic resource definition
 	Extension []*FHIRExtension `json:"extension,omitempty"`
